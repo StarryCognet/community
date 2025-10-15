98fa881a4c12ba91c1c98e172e64cececdb1ab50
StarryCognet <3398377037@qq.com>
2025-10-15 08:27:28 +0800

    feat(AdministratorView): 添加弹窗表单功能并完善数据结构
    在管理员视图中添加了一个可弹出的表单对话框，用于输入活动名称和选择活动区域。
    同时为组件增加了必要的数据属性，包括表单字段、可见性控制变量以及示例表格数据。

--- src/components/AdministratorView.vue
+++ src/components/AdministratorView.vue
@@ -10,7 +10,24 @@
       </el-form-item>
     </el-form>
     <el-row>
-      <el-button type="primary">主要按钮</el-button>
+      <el-button type="primary" @click="dialogFormVisible = true">主要按钮</el-button>
+      <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+          <el-form-item label="活动名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="活动区域" :label-width="formLabelWidth">
+            <el-select v-model="form.region" placeholder="请选择活动区域">
+              <el-option label="区域一" value="shanghai"></el-option>
+              <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+        </div>
+      </el-dialog>
     </el-row>
     <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55"> </el-table-column>
@@ -58,6 +75,42 @@ export default {
   },
   data() {
     return {
+      gridData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+      ],
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: '',
+      },
+      formLabelWidth: '120px',
+
       currentPage1: 5,
       currentPage2: 5,
       currentPage3: 5,
