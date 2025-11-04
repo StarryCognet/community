189a10fcf55d39e58af2d901d61ccf6db2cf91b8
firefly <2778596933@qq.com>
2025-11-04 15:24:31 +0800

    会员设置

---
+++ pages/pagesmember/member-settings/member-settings.vue
@@ -0,0 +1,59 @@
+<template>
+	<view>
+		<div class="conmt">
+			<div class="conmt-top">
+				<div class="conmt-box">
+					<span style="color: #cc7059"><</span>
+					<p>dadw</p>
+				</div>
+				<div class="box">
+					<span>></span>
+				</div>
+			</div>
+			<div class="conmt-top">
+				<div class="conmt-box">
+					<span style="color: #3dce6d"><</span>
+					<p>dadw</p>
+				</div>
+				<div class="box">
+					<span>></span>
+				</div>
+			</div>
+		</div>
+		<div class="cont">
+			<div class="cont-top">
+				<div class="cont-box">
+					<span style="color: #359bec"><</span>
+					<p>dadw</p>
+				</div>
+				<div class="box">
+					<p>dawd</p>
+					<span>></span>
+				</div>
+			</div>
+			<div class="cont-top">
+				<div class="cont-box">
+					<span style="color: #75dc98"><</span>
+					<p>dadw</p>
+				</div>
+				<div class="box">
+					<p>dawd</p>
+					<span>></span>
+				</div>
+			</div>
+		</div>
+		<button>退出当前账号</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less">
+@import url(../../../static/css/member-settings.less);
+</style>
