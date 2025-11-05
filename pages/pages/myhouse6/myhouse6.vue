139de312ce56a0b89fcf28e7b58462500fec601d
firefly <2778596933@qq.com>
2025-11-05 17:03:03 +0800

    拉取

---
+++ pages/pages/myhouse6/myhouse6.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<uni-nav-bar left-icon="left" left-text="返回" title="房屋认证" />
+		<div class="conmt">
+			<div class="box">
+				<image src="/static/img/微信图片_2025-11-05_165231_126.png" mode=""></image>
+			</div>
+			<div class="box-zi">
+				<p class="zi">尊敬的13522566761用户</p>
+				<p>您的房屋正在审核中...</p>
+				<p>请等待社区管理人员审核!</p>
+			</div>
+		</div>
+		<button>查看房屋认证情况</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less">
+@import url(../../../static/css/addmyhouse3.less);
+</style>
