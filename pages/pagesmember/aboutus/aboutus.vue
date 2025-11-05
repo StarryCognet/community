1167233636113ba82f08c8a5a10150b9428c8f03
firefly <2778596933@qq.com>
2025-11-05 09:07:05 +0800

    关于我们

---
+++ pages/pagesmember/aboutus/aboutus.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<uni-nav-bar left-icon="left" left-text="返回" title="关于我们" />
+		<div class="conmt">
+			<image src="../../../static/img/aboutus.png" mode=""></image>
+			<p>扫一扫下载 甜城app</p>
+		</div>
+		<div class="conent"></div>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less">
+@import url(../../../static/css/aboutus.less);
+</style>
