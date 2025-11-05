b9ea916755c9c856660c02961e0ccbe361e5508d
firefly <2778596933@qq.com>
2025-11-05 15:48:13 +0800

    门店列表

---
+++ pages/pages/storelist/storelist.vue
@@ -0,0 +1,99 @@
+<template>
+	<view>
+		<uni-nav-bar left-icon="left" left-text="返回" title="门店列表" />
+		<div class="conmt">
+			<div class="head">
+				<div class="head-left">
+					<span>搜索关键字</span>
+					<uni-icons class="biao" custom-prefix="iconfont" type="icon-sousuo" size="31rpx"></uni-icons>
+				</div>
+				<div class="head-reight">
+					<span>筛选</span>
+					<uni-icons class="biao" custom-prefix="iconfont" type="icon-shaixuan" size="31rpx"></uni-icons>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+			<div class="conent">
+				<div class="conent-box">
+					<image src="/static/img/nr-xc.png" mode=""></image>
+					<div class="con-zi">
+						<h2>吉野家牛肉饭</h2>
+						<p>美食 | 午饭</p>
+						<div>查看详情</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less">
+@import url(../../../static/css/storelist.less);
+</style>
